1bc3bb6fce57ab18dcb972b31c34faded1938376
tocharomera <nevertalking@hotmail.com>
2018-02-04 17:43:51 +0100

    functioning

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var consolidate = require("consolidate"); //1
 var _ = require("underscore");
 var bodyParser = require('body-parser');
-
-var routes = require('./routes'); //File that contains our endpoints
-var mongoClient = require("mongodb").MongoClient;
+//File that contains our endpoints
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/places");
 
 var app = express();
 app.use(bodyParser.urlencoded({
@@ -29,12 +29,28 @@ var id = 0;
 var coordinates = 0
 server.listen(portNumber, function() { //Runs the server on port 8000
   console.log('Server listening at port ' + portNumber);
-  var coord = "00"
-  var userId
-  var url = 'mongodb://localhost:27017/places'; //Db name
-  mongoClient.connect(url, function(err, database) { //a connection with the mongodb is established here.
+  var placeSchema = new mongoose.Schema({
+    "type": "",
+    "name": "",
+    "description": "",
+    "phone": "",
+    "email": "",
+    "address": "",
+    "location": {
+      "type": "",
+      "coordinates": [
+        0,
+        0
+      ]
+    }
+  });
+  var place = mongoose.model("place", placeSchema, "markers");
+
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() { //a connection with the mongodb is established here.
     console.log("Connected to Database");
-    db = database
+     var code = 01
     app.get('/', function(req, res) {
       io.on('connection', function(socket) { //Listen on the 'connection' event for incoming sockets
         console.log('A user just connected');
@@ -42,28 +58,50 @@ server.listen(portNumber, function() { //Runs the server on port 8000
           socket.join(data.userId); //User joins a unique room/channel that's named after the userId
           console.log("User joined room: " + data.userId);
         });
-  
-      //console.log(db.collection("markers").count())
-      db.collection("markers").find({},{}, function(err, docs) {
-        docs.each(function(err, doc) {
-          if (doc) {
-            id = doc.id
-            coordinates = doc.location.coordinates
-            console.log(coordinates+",end");
-            socket.emit("newmarker", {coordinates: coordinates+",end"})
-          }
+
+        //console.log(db.collection("markers").count())
+        db.collection("markers").find({}, {}, function(err, docs) {
+          docs.each(function(err, doc) {
+            if (doc) {
+              code = doc
+              socket.emit("newmarker", code)
+            }
+          });
         });
-      });
+        socket.on("savemarker", function(newmarker) {
+          console.log("times");
+          var data = JSON.parse(newmarker)
+
+          var newPlace = new place({
+            "type": data.type,
+            "name": data.name,
+            "description": data.description,
+            "phone": data.phone,
+            "email": data.email,
+            "address": data.address,
+            "location": data.location
+          })
+
+          newPlace.save(function(error) {
+
+            if (error) {
+              console.error(error);
+            }
+          })
+        })
+
+        socket.on('leave', function(data) { //Listen to any join event from connected users
+          socket.leave(data.userId); //User joins a unique room/channel that's named after the userId
+          console.log("User left room: " + data.userId);
+        });
       });
       res.render('index.html', {
 
-        userId: id,
-        coord: coordinates
+        code: code
 
       });
+
     });
-
-
 
 
   });
